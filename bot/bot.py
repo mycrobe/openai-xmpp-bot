7f253e82c2f80457a40e779f19b75bc56f12416b
Joe Mulvaney <me@mycro.be>
2023-03-30 14:40:33 -0400

    works

--- bot/bot.py
+++ bot/bot.py
@@ -1,55 +1,66 @@
-import aiohttp
-import asyncio
 import slixmpp
+import openai
+from dotenv import load_dotenv
+import os
 
-class MyBot(slixmpp.ClientXMPP):
+load_dotenv()
 
-    def __init__(self, jid, password):
+class OpenAIBot(slixmpp.ClientXMPP):
+
+    def __init__(self):
+        jid = os.environ.get('JID')
+        password = os.environ.get('PASSWORD')
+        openai.api_key = os.environ.get('OPENAI_API_KEY')
+        self.model = os.environ.get('MODEL')
         super().__init__(jid, password)
         self.add_event_handler("session_start", self.start)
-        self.headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer YOUR_API_KEY"
-        }
-        self.api_endpoint = "https://api.openai.com/v1/completions"
-        self.paragraph_separator = "\n\n"
+        self.add_event_handler("message", self.message)
+        self.messages = {}
 
-    def start(self, event):
+
+    def start(self, _):
         self.send_presence()
         self.get_roster()
 
-    async def generate_text(self, prompt):
-        async with aiohttp.ClientSession() as session:
-            data = {
-                "prompt": prompt,
-                "temperature": 0.7,
-                "stop": ["\n"]
-            }
-            async with session.post(self.api_endpoint, headers=self.headers, json=data) as response:
-                response_json = await response.json()
-                generated_text = response_json["choices"][0]["text"]
-                paragraphs = generated_text.split(self.paragraph_separator)
-                for paragraph in paragraphs:
-                    yield paragraph.strip()
 
-    async def send_paragraphs(self, msg, generator):
-        chunks = []
-        async for paragraph in generator:
-            chunks.append(paragraph)
-            if len("\n".join(chunks)) >= 500:
-                self.send_message(mto=msg['from'], mbody="\n".join(chunks))
-                chunks = []
-        if len(chunks) > 0:
-            self.send_message(mto=msg['from'], mbody="\n".join(chunks))
-
+    def generate_text(self, prompt, sender):
+        messages = self.get_messages(sender)
+        messages.append({"role": "user", "content": prompt})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=messages,
+            temperature=0.5
+        )
+        message = response['choices'][0]['message']
+        messages.append(message)
+        self.send_message(mto=sender, mbody=message['content'])
+     
+     
+    def get_messages(self, user):
+        if not self.messages.get(user):
+            self.reset_messages(user)
+        
+        return self.messages[user]
+    
+    
+    def reset_messages(self, user):
+        self.messages[user] = [{"role": "system", "content": "You are a helpful assistant."}]
+    
+    
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
             prompt = msg['body']
-            loop = asyncio.get_event_loop()
-            task = loop.create_task(self.generate_text(prompt))
-            loop.create_task(self.send_paragraphs(msg, task))
+            sender = msg['from']
+            
+            if prompt == 'reset':
+                self.reset_messages(sender)
+                self.send_message(mto=sender, mbody='All is forgotten!')
+            
+            else:
+                self.generate_text(prompt, sender)
+
 
 if __name__ == '__main__':
-    xmpp = MyBot("user@domain.com", "password")
-    xmpp.connect()
-    xmpp.process(forever=False)
+    bot = OpenAIBot()
+    bot.connect()
+    bot.process(forever=False)
