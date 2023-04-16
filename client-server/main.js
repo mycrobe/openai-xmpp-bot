a417c27e242c1f2e4a43448315e3ea1ac57894d7
Joseph Mulvaney <joseph.mulvaney@schrodinger.com>
2023-04-16 15:21:31 -0400

    cleanup the gpt bot slightly

--- client-server/main.js
+++ client-server/main.js
@@ -5,7 +5,9 @@ import Bot, { EchoBot } from './Bot.js';
 import GptBot from './GptBot.js';
 import imessageManager from './ImessageManager.js';
 
-const gpt = await Bot.forUser('gpt', GptBot);
+const gpt = await Bot.forUser('gpt35', GptBot);
+await gpt.setFullname('GPT 3.5 Turbo');
+await gpt.presenceSubscribe('joe@dockerpi.local');
 
 imessageManager.start();
 const conversations = await imessageManager.getConversations();
